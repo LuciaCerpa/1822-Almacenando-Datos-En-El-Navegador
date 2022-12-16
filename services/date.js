65abd32aacbb4a6a8f7eb0e8ec4ed5c533d27d77
lucy1528 <luxysm@gmail.com>
2022-12-16 15:48:47 -0600

    tasks js

---
+++ services/date.js
@@ -0,0 +1,20 @@
+export const uniqueDates = (tasks) => {
+    const unique = [];
+
+    tasks.forEach(task => {
+        if (!unique.includes(task.dateFormat)) {
+            unique.push(task.dateFormat);
+        }
+    });
+
+    return unique;
+};
+
+
+export const orderDates = (dates) => {
+    return dates.sort((a, b) => {
+        const firstDate = moment(a, "DD/MM/YYYY");
+        const SecondDate = moment(a, "DD/MM/YYYY");
+        return firstDate - SecondDate;
+    });
+}
